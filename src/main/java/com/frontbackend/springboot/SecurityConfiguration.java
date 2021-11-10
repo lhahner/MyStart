306326f4f275881422af68e217db3c9868b14dfa
ggkhrmv <gg.khrmv@gmail.com>
2021-11-10 10:03:30 +0100

    added Security

---
+++ src/main/java/com/frontbackend/springboot/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package com.frontbackend.springboot;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
